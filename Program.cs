240ed3ba22231bca2c842ecefede926c6ed53c36
Ekrem Sağlam <ekremsaglammm@gmail.com>
2021-07-29 19:58:18 +0300

    Final Commit

--- Program.cs
+++ Program.cs
@@ -6,7 +6,21 @@ namespace conditions
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int time = DateTime.Now.Hour;
+            if (6<time && time <11){
+                Console.WriteLine("Günaydın");
+            }
+            else if (time <18 ){
+                Console.WriteLine("İyi günler");
+            }
+            else if (time>18 ){
+                Console.WriteLine("İyi geceler");
+            }
+            string sonuc;
+            //sonuc=time<18 ? "iyi günler" : "İyi geceler";
+
+            sonuc = time<11 && time>=6 ? "Günaydın" : time<=18 ? "İyi Günler" : "İyi Geceler"; 
+            Console.WriteLine(sonuc);
         }
     }
 }
